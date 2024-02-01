a1e90c87fb5fbba0081cd98e03ee566a87be1b5d
pmt1506 <pmtien150603@gmail.com>
2024-02-01 08:28:50 +0700

    add footer

--- ftravel/src/components/Footer.js
+++ ftravel/src/components/Footer.js
@@ -1,16 +1,31 @@
+import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 const Footer = () => {
     return (
-        <Row className='header-app fixed-bottom'>
-            <Container className='footer-component'>
+        <Container className='footer-app'>
+            <Row className='footer-component'>
                 <Row>
-                    <Col xs={12}>
-                        @Copyright by Group 3
+                    <Col md={4}>
+                        <h5>FTravel</h5>
+                        <p>Welcome to FTravel - Your Ultimate Travel Companion. Explore the world with us!</p>
+                    </Col>
+                    <Col md={4}>
+                        <h5>Quick Links</h5>
+                        <ul>
+                            <li><a href='/'>Tours</a></li>
+                            <li><a href='/'>Hotels</a></li>
+                            <li><a href='/'>Events</a></li>
+                        </ul>
+                    </Col>
+                    <Col md={4}>
+                        <h5>Contact Us</h5>
+                        <p>Email: info@ftravel.com</p>
+                        <p>Phone: +1 (123) 456-7890</p>
                     </Col>
                 </Row>
-            </Container>
-        </Row>
+            </Row>
+        </Container>
     );
 }
 
