a925ccb5966f7c9698f60800d56f522d556a3dc3
pmt1506 <pmtien150603@gmail.com>
2024-02-19 09:42:54 +0700

    add bill route to server

--- API/server.js
+++ API/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import connectDB from "./database.js";
 
+import { billRouter } from "./routes/index.js";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,6 +15,9 @@ const PORT = process.env.PORT;
 app.get("/", (res, req) => {
   res.send("welcome kkk");
 });
+
+app.use(`/bill`, billRouter);
+
 app.listen(PORT, async () => {
   connectDB();
   console.log(`Server is running on port ${PORT} `);
