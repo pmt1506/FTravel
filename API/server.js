88f285ff31b5352b748510d462ef8b9a4e95f274
Bui Tien Anh <tienanhbkp03@gmail.com>
2024-02-19 20:49:08 +0700

    Fix server

--- API/server.js
+++ API/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import connectDB from "./database.js";
 
+import { reportRouter } from "../API/routes/report.js";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,6 +15,9 @@ const PORT = process.env.PORT;
 app.get("/", (res, req) => {
   res.send("welcome kkk");
 });
+
+app.use(`/report`, reportRouter);
+
 app.listen(PORT, async () => {
   connectDB();
   console.log(`Server is running on port ${PORT} `);
