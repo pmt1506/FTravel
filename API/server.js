4578a5522145fd07ef5b97f57d8bf37c994036f3
dungxtran <dungxtran27@gmail.com>
2024-02-21 23:11:26 +0700

    ko sua j dau sodi

--- API/server.js
+++ API/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 
-app.use(`/carts`, cartRouter)
+app.use(`/carts`, cartRouter);
 const PORT = process.env.PORT;
 
 app.get("/", (res, req) => {
