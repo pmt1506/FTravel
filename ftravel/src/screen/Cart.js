379df1f61ddfedec448dcf153f8a14f026655318
Nguyễn Đức Minh Thành <minhthanh22032003@gmail.com>
2024-03-02 23:45:44 +0700

    update frontend

--- ftravel/src/screen/Cart.js
+++ ftravel/src/screen/Cart.js
@@ -17,11 +17,43 @@ const Cart = () => {
     }, []);
 
     return (
-        <Container>
+        <Container className="container-fluid col-lg-8">
             <Row>
-                <p>Hello</p>
+                <a href="/" style={{ textDecoration: "none", color: "black" }} className="fa fa-home">
+                    <i class="bi bi-house"></i>Home</a>
+                <i class="bi bi-chevron-compact-right"></i>
+                <p><i class="bi bi-cart"></i>Cart</p>
             </Row>
-        </Container>
+            <Row>
+                <h2>Shopping Cart</h2>
+            </Row>
+            <Row style={{ padding: "15px", backgroundColor: "#fff" }}>
+                <Table className="table-striped table-bordered table-responsive mt-5">
+                    <thead>
+                        <th className="col-md-1">Type</th>
+                        <th className="col-md-4">Title</th>
+                        <th className="col-md-3">Start date</th>
+                        <th className="col-md-1">Slot</th>
+                        <th className="col-md-1">Price</th>
+                        <th className="col-md-2">Action</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tour</td>
+                            <td>This tour is for fvking loser</td>
+                            <td>22/03/2344</td>
+                            <td>1</td>
+                            <td>200000VND</td>
+                            <td>
+                                <Button className="btn-danger" style={{ width: "80px", fontSize: "14px", padding: "2px" }}>Delete</Button>
+                                <Button className="btn-primary" style={{ width: "80px", fontSize: "14px", padding: "2px" }}>Buy</Button>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Row>
+        </Container >
     )
 }
 
--- ftravel/src/screen/Home.js
+++
@@ -1,8 +0,0 @@
-import { Row, Container, Col, Button } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-
-const Home = () => {
-    <NavLink to={'/cart'} className="align-items-center">
-        <Button variant="outline-success">Cart</Button>
-    </NavLink>
-}
