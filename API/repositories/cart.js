44130176d1e9352e4f5fbb22f0dbeeb85b2b175b
Nguyễn Đức Minh Thành <minhthanh22032003@gmail.com>
2024-03-04 11:58:14 +0700

    update

--- API/repositories/cart.js
+++ API/repositories/cart.js
@@ -12,6 +12,7 @@ const addToCart = async ({ userID, serviceID }) => {
 const viewCart = async (userID) => {
     try {
         const populatedCart = await Carts.findOne({ userID: userID })
+            .populate("serviceID")
             .exec();
 
         return populatedCart;
