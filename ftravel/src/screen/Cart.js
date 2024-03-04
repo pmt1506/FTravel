cff411b8e8d3845a4dac029bba68889070cc1262
Nguyễn Đức Minh Thành <minhthanh22032003@gmail.com>
2024-03-04 16:15:42 +0700

    update

--- ftravel/src/screen/Cart.js
+++ ftravel/src/screen/Cart.js
@@ -1,7 +1,7 @@
 import { Link, useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { Button, Col, Container, Form, FormControl, Row, Table } from "react-bootstrap"
-import SideBar from "../components/Sidebar.js";
+import SideBar from "../components/Sidebar";
 
 const Cart = () => {
     const { userId } = useParams();
@@ -47,13 +47,13 @@ const Cart = () => {
                     <SideBar />
                 </Col>
                 <Col xs={9}>
-                    <Row>
+                    <Row style={{ justifyContent: 'left !important' }} className="ml-3">
                         <a href="/" style={{ textDecoration: "none", color: "black" }} className="fa fa-home">
                             <i class="bi bi-house"></i>Home</a>
                         <i class="bi bi-chevron-compact-right"></i>
                         <p><i class="bi bi-cart"></i>Cart</p>
                     </Row>
-                    <Row>
+                    <Row className="ml-3">
                         <h2>Shopping Cart</h2>
                     </Row>
                     <Row style={{ padding: "15px", backgroundColor: "#fff" }}>
@@ -64,10 +64,10 @@ const Cart = () => {
                                 <th className="col-md-3">Start date</th>
                                 <th className="col-md-1">Slot</th>
                                 <th className="col-md-1">Price</th>
-                                <th className="col-md-2">Action</th>
+                                <th className="col-md-1">Action</th>
                             </thead>
                             <tbody>
-                                {service.map((s) => {
+                                {service.map((s) => (
                                     <tr key={s._id}>
                                         <td>{s.type.serviceName}</td>
                                         <td>
@@ -90,7 +90,7 @@ const Cart = () => {
                                             </Button>
                                         </td>
                                     </tr>
-                                })}
+                                ))}
                             </tbody>
                         </Table>
                     </Row>
