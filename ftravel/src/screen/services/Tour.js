23ca3b4558a0516d38efbb3173bba24ce5110b49
pmt1506 <pmtien150603@gmail.com>
2024-03-05 22:04:35 +0700

    rearrange tour banner

--- ftravel/src/screen/services/Tour.js
+++ ftravel/src/screen/services/Tour.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Slider from "react-slick";
 import "../../css/services.css";
+import TourBanner from "../../components/Tour/TourBanner";
 
 const Tour = () => {
   const [tourList, setTourList] = useState([]);
@@ -77,29 +77,6 @@ const Tour = () => {
     fetchTourCount();
   }, []);
 
-  const placeholderImages = [
-    "https://images.pexels.com/photos/13588741/pexels-photo-13588741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/626749/pexels-photo-626749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/2649403/pexels-photo-2649403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/629167/pexels-photo-629167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/458917/pexels-photo-458917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/12485969/pexels-photo-12485969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/12360777/pexels-photo-12360777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/1033729/pexels-photo-1033729.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/1107717/pexels-photo-1107717.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/449283/pexels-photo-449283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  ];
-
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    fade: true,
-    cssEase: "linear",
-  };
-
   // Constant demo rating value
   const demoRating = 4;
 
@@ -125,28 +102,7 @@ const Tour = () => {
 
   return (
     <>
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col">
-            <Slider {...settings}>
-              {placeholderImages.map((image, index) => (
-                <div key={index}>
-                  <img
-                    src={image}
-                    alt={`slide ${index + 1}`}
-                    className="img-fluid"
-                    style={{
-                      height: "500px",
-                      width: "100%",
-                      objectFit: "cover",
-                    }}
-                  />
-                </div>
-              ))}
-            </Slider>
-          </div>
-        </div>
-      </div>
+      <TourBanner />
       <div className="container mt-4">
         <div className="row">
           <div className="col-md-4">
