daa2bcab92d49b1ecb8d8b2a1012dee4e4ccf30d
dungxtran <dungxtran27@gmail.com>
2024-03-06 20:50:10 +0700

    done fe login

--- ftravel/src/components/Header.js
+++ ftravel/src/components/Header.js
@@ -1,28 +1,48 @@
-import { Row, Container, Col, Button } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import logo from '../img/logo.png';
-
+import { Row, Container, Col, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import logo from "../img/logo.png";
+import LoginModal from "./LoginModal";
+import { useState } from "react";
 const Header = () => {
-    return (
-        <Container className='header-app'>
-            <Row>
-                <Col xs={3} className="header-left">
-                    <NavLink to={'/'} className="d-flex align-items-center" style={{ textDecoration: "none" }}>
-                        <img src={logo} alt="logo" width={100} height={100} />
-                        <h1>FTravel</h1>
-                    </NavLink>
-                </Col>
-                <Col xs={9} style={{ textAlign: 'right', marginTop: "30px" }} className="header-right">
-                    <NavLink to={'/login'} className="align-items-center">
-                        <Button variant="outline-primary" className="mx-2">Login</Button>
-                    </NavLink>
-                    <NavLink to={'/register'} className="align-items-center">
-                        <Button variant="outline-success">Register</Button>
-                    </NavLink>
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleCloseLogin = () => setShowLogin(false);
+  const handleShowLogin = () => setShowLogin(true);
+  return (
+    <Container className="header-app">
+      <Row>
+        <Col xs={3} className="header-left">
+          <NavLink
+            to={"/"}
+            className="d-flex align-items-center"
+            style={{ textDecoration: "none" }}
+          >
+            <img src={logo} alt="logo" width={100} height={100} />
+            <h1>FTravel</h1>
+          </NavLink>
+        </Col>
+        <Col
+          xs={9}
+          style={{ textAlign: "right", marginTop: "30px" }}
+          className="header-right"
+        >
+          <NavLink className="align-items-center">
+            <Button
+              variant="outline-primary"
+              className="mx-2"
+              onClick={handleShow}
+            >
+              Login
+            </Button>
+          </NavLink>
+          <NavLink to={"/register"} className="align-items-center">
+            <Button variant="outline-success">Register</Button>
+          </NavLink>
+        </Col>
+      </Row>
+      <LoginModal show={showLogin} setShow={setShowLogin} />
+    </Container>
+  );
+};
 
 export default Header;
---
+++ ftravel/src/components/LoginModal.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+function LoginModal({ show, setShowLogin }) {
+  const handleCloseLogin = () => setShowLogin(false);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Add logic to handle form submission (e.g., login request)
+    // Retrieve form data using e.target.elements
+    // Example: const email = e.target.elements.email.value;
+    // Close the modal after submission: handleClose();
+    handleClose();
+  };
+
+  return (
+    <Modal show={show}>
+      <Modal.Header>
+        <Modal.Title>Login</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group controlId="email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              required
+            />
+          </Form.Group>
+          {/* Add password input field here */}
+        </Form>
+        <hr />
+        <div className="justify-content-center">or login with google</div>
+        <Button className="btn-btn-success">Google</Button>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleCloseLogin}>
+          Close
+        </Button>
+        <Button variant="primary" type="submit" onClick={handleFormSubmit}>
+          Log In
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default LoginModal;
