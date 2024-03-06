3a04fa1057050f67ec961972a6b6497d2347c50c
Bui Tien Anh <tienanhbkp03@gmail.com>
2024-03-06 21:29:26 +0700

    add part 2

--- ftravel/src/App.js
+++ ftravel/src/App.js
@@ -2,7 +2,15 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import NavbarComponent from "./components/Navbar";
+// import NavbarComponent from "./components/Navbar";
+import Home from "./screen/Home";
+import About from "./screen/About";
+import Cart from "./screen/Cart";
+import Services from "./screen/dashboard/Services";
+import Users from "./screen/dashboard/Users";
+import Reports from "./screen/dashboard/Reports";
+import Tour from "./screen/services/Tour";
+import EditProfile from "./screen/dashboard/EditProfile";
 
 function App() {
   return (
@@ -11,9 +19,11 @@ function App() {
       <NavbarComponent />
       <Routes>
         <Route path="" element />
-        <Route path="" element />
-        <Route path="" element />
-        <Route path="" element />
+
+        <Route path="/dashboard/service" element={<Services/>} />
+        <Route path="/dashboard/user" element={<Users/>} />
+        <Route path="/dashboard/report" element={<Reports/>} />
+        <Route path="/dashboard/edit" element={<EditProfile/>} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- ftravel/src/screen/dashboard/Users.js
+++ ftravel/src/screen/dashboard/Users.js
@@ -1,18 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import DashboardTemplate from '../../template/DashboardTemplate'
-import { Col, Row } from 'react-bootstrap'
-
+import { Row } from 'react-bootstrap'
+import axios from 'axios';
 
 const Users = () => {
-  
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    // Fetch user data from your API endpoint
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get('http://localhost:9999/account/:accID'); // Replace with your API endpoint
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <DashboardTemplate title="Manage User">
       <Row className="m-3">
-        Users
+        <div>
+          <h1>User Profile</h1>
+          <img src={userData.avatarIMG} alt="User Avatar" />
+          <p>User Name: {userData.userName}</p>
+          <p>Email: {userData.email}</p>
+          <p>Phone Number: {userData.phoneNumber}</p>
+          <p>Address: {userData.address}</p>
+          <p></p>
+        </div>
       </Row>
-
     </DashboardTemplate>
   )
 }
 
-export default Users
+export default Users;
