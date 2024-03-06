8c40cb1baddcb35860ef6c0f06674bc810221256
Bui Tien Anh <tienanhbkp03@gmail.com>
2024-03-06 22:00:40 +0700

    add2

--- ftravel/src/screen/dashboard/Users.js
+++ ftravel/src/screen/dashboard/Users.js
@@ -28,13 +28,14 @@ const Users = () => {
     <DashboardTemplate title="Manage User">
       <Row className="m-3">
         <div>
-          
+
           <h1>User Profile</h1>
           <p/><img src={userData.avatarIMG} alt="User Avatar" />
           <p>User Name: {userData.userName}</p>
           <p>Email: {userData.email}</p>
           <p>Phone Number: {userData.phoneNumber}</p>
           <p>Address: {userData.address}</p>
+          {/* From tienanh with love */}
           <p></p>
         </div>
       </Row>
