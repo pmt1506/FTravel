6b438dc1cd459c53dbe39e752b0109d9c3d5cb04
hienhauvu <hauvthhe170070@fpt.edu.vn>
2024-03-13 19:14:01 +0700

    Admin Service - pagination and search

--- ftravel/src/screen/dashboard/AdminServices.js
+++ ftravel/src/screen/dashboard/AdminServices.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import DashboardTemplate from '../../template/DashboardTemplate'
-import { Col, Row } from 'react-bootstrap'
+import { Button, Col, Form, Row } from 'react-bootstrap'
 import '../../css/DashboardServices.css'
 
 const Services = () => {
@@ -8,6 +8,11 @@ const Services = () => {
   const [tourList, setTourList] = useState([]);
   const [hotelList, setHotelList] = useState([]);
   const [eventList, setEventList] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   const tourListID = "65d440dd4ba915fa5c498398";
   const hotelListID = "65e2e9c5d9e75d25d6a2b090";
@@ -19,8 +24,20 @@ const Services = () => {
     "65e2e9c5d9e75d25d6a2b090": "Hotel",
     "65e2e9d2d9e75d25d6a2b092": "Event"
   };
+
   useEffect(() => {
-    fetch("http://localhost:9999/service/all?pageSize=5")
+    fetchData();
+  }, [currentPage]); // Fetch data when currentPage changes
+
+  useEffect(() => {
+    const results = allServices.filter(service =>
+      service.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm, allServices]);
+
+  const fetchData = () => {
+    fetch(`http://localhost:9999/service/all?pageSize=${itemsPerPage}&pageNumber=${currentPage}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
@@ -36,80 +53,157 @@ const Services = () => {
           data.allServices.filter((service) => service.type === eventListID)
         );
       });
-  }, []);
+  };
+
+  // Tính index bắt đầu của mục đầu tiên trên trang hiện tại
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = allServices.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+
+  // Searching
+  const handleSearch = () => {
+    setSearchResults(allServices.filter(service =>
+      service.title.toLowerCase().includes(searchTerm.toLowerCase())
+    ));
+  };
+
+  const highlightText = (text, highlight) => {
+    const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+    return (
+      <span>
+        {parts.map((part, i) => (
+          <span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? { backgroundColor: 'yellow' } : {}}>
+            {part}
+          </span>
+        ))}
+      </span>
+    );
+  };
+
   return (
     <DashboardTemplate title="Manage Service">
-      <Row className="m-3">
-        Action Bar
+      <Row className="m-3 ml-auto">
+        <Col >
+          <Form.Control style={{width: "20rem"}}
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)
+            }
+          />
+          {/* <Button onClick={handleSearch}>Search</Button> */}
+        </Col>
+        <Col >
+          <ul className="pagination">
+            {Array.from({ length: Math.ceil((searchTerm ? searchResults.length : allServices.length) / itemsPerPage) }).map((_, index) => (
+              <li key={index} className="page-item">
+                <button onClick={() => paginate(index + 1)} className="page-link">
+                  {index + 1}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </Col>
       </Row>
       <Row className="m-3">
-        {allServices.map(service => (
-          <div className="item-list" key={service._id}>
-            <Row>
-              <Col md={3}>
-                <div className="featured">Đặc sắc</div>
-                <div className="thumb-image">
-                  <a href="#" target="_blank">
-                    <img src={service.thumbnail} className="img-responsive" alt="" />
-                  </a>
-                </div>
-              </Col>
-              <Col md={9}>
-                <div className="item-title">
-                  <a href="https://ongvove.com/tour/kham-pha-tour-hon-thom-phu-quoc" target="_blank">
-                    {service.title}
-                  </a>
-                </div>
-                <div className="item-title2">
-                  <i className="icofont-license"></i>
-                  Kiểu dịch vụ: <span className="badge badge-info">
-                  {/* {console.log(service.type.serviceName)}
-                  {service.type} */}
-                  {serviceTypes[service.type]}
-                  </span>
-                </div>
-                <div className="item-title2">
-                  <i className="icofont-paper-plane"></i>
-                  Địa điểm: {service.city}
-                </div>
-                <div className="item-title2">
-                  <i className="icofont-money"></i>
-                  Giá bán: <span className="sale-price"></span> <span className="price">{service.price}</span>
-                </div>
-                <div className="item-title2 rate">
-                  <i className="icofont-badge"></i>
-                  <div className="service-review tour-review-0">
-                    <span className="review">
-                      0 Đánh giá
-                    </span>
-                    <div className="list-star">
-                      {/* <ul className="booking-item-rating-stars row">
-                      <li><i className="fa fa-star-o"></i></li>
-                      <li><i className="fa fa-star-o"></i></li>
-                      <li><i className="fa fa-star-o"></i></li>
-                      <li><i className="fa fa-star-o"></i></li>
-                      <li><i className="fa fa-star-o"></i></li>
-                    </ul>
-                    <div className="booking-item-rating-stars-active row" style={{ width: "0%" }}>
-                      <ul className="booking-item-rating-stars">
-                        <li><i className="fa fa-star"></i></li>
-                        <li><i className="fa fa-star"></i></li>
-                        <li><i className="fa fa-star"></i></li>
-                        <li><i className="fa fa-star"></i></li>
-                        <li><i className="fa fa-star"></i></li>
-                      </ul>
-                    </div> */}
+        {searchTerm ? (
+          searchResults.length > 0 ? (
+            searchResults.map(service => (
+              <div className="item-list" key={service._id}>
+                <Row>
+                  <Col md={3}>
+                    <div className="featured">Đặc sắc</div>
+                    <div className="thumb-image">
+                      <a href="#" >
+                        <img src={service.thumbnail} className="img-responsive" alt="" />
+                      </a>
+                    </div>
+                  </Col>
+                  <Col md={9}>
+                    <div className="item-title">
+                      <a href="#" target="_blank">
+                        {highlightText(service.title, searchTerm)}
+                      </a>
+                    </div>
+                    <div className="item-title2">
+                      <i className="icofont-license"></i>
+                      Kiểu dịch vụ: <span className="badge badge-info">{serviceTypes[service.type]}</span>
+                    </div>
+                    <div className="item-title2">
+                      <i className="icofont-paper-plane"></i>
+                      Địa điểm: {service.city}
+                    </div>
+                    <div className="item-title2">
+                      <i className="icofont-money"></i>
+                      Giá bán: <span className="sale-price"></span> <span className="price">{service.price}</span>
+                    </div>
+                    <div className="item-title2 rate">
+                      <i className="icofont-badge"></i>
+                      <div className="service-review tour-review-0">
+                        <span className="review">
+                          0 Đánh giá
+                        </span>
+                      </div>
+                    </div>
+                    <div className="control-action">
+                      <a href="#" className="btn btn-danger">Hide</a>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            ))
+          ) : (
+            <div>No services found</div>
+          )
+        ) : (
+          currentItems.map(service => (
+            <div className="item-list" key={service._id}>
+              <Row>
+                <Col md={3}>
+                  <div className="featured">Đặc sắc</div>
+                  <div className="thumb-image">
+                    <a href="#" >
+                      <img src={service.thumbnail} className="img-responsive" alt="" />
+                    </a>
+                  </div>
+                </Col>
+                <Col md={9}>
+                  <div className="item-title">
+                    <a href="#">
+                      {service.title}
+                    </a>
+                  </div>
+                  <div className="item-title2">
+                    <i className="icofont-license"></i>
+                    Kiểu dịch vụ: <span className="badge badge-info">{serviceTypes[service.type]}</span>
+                  </div>
+                  <div className="item-title2">
+                    <i className="icofont-paper-plane"></i>
+                    Địa điểm: {service.city}
+                  </div>
+                  <div className="item-title2">
+                    <i className="icofont-money"></i>
+                    Giá bán: <span className="sale-price"></span> <span className="price">{service.price}</span>
+                  </div>
+                  <div className="item-title2 rate">
+                    <i className="icofont-badge"></i>
+                    <div className="service-review tour-review-0">
+                      <span className="review">
+                        0 Đánh giá
+                      </span>
                     </div>
                   </div>
-                </div>
-                <div className="control-action">
-                  {/* <a href="#" className="btn btn-info">View</a> */}
-                  <a href="#" className="btn btn-danger">Hide</a>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        ))}
+                  <div className="control-action">
+                    <a href="#" className="btn btn-danger">Hide</a>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          ))
+        )}
       </Row>
 
     </DashboardTemplate>
