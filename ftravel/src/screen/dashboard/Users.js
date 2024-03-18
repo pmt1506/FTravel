f3b355aad3227a382cbd63a988b34b7042373ec7
Bui Tien Anh <tienanhbkp03@gmail.com>
2024-03-18 22:14:01 +0700

    Fix Users.js

--- ftravel/src/screen/dashboard/Users.js
+++ ftravel/src/screen/dashboard/Users.js
@@ -20,16 +20,19 @@ const Users = () => {
   return (
     <DashboardTemplate title="Manage User">
       <Row className="m-3">
-        <div>
-          <h1>User Profile</h1>
-          <p />
-          <img src={userData.avatarIMG} alt="User Avatar" />
-          <p>User Name: {userData.userName}</p>
-          <p>Email: {userData.email}</p>
-          <p>Phone Number: {userData.phoneNumber}</p>
-          <p>Address: {userData.address}</p>
-          {/* From tienanh with love */}
-          <p></p>
+        <div className="container" style={{ alignContent: "center", marginLeft: "300px" }}>
+          <h2 style={{ textAlign: "center" }}>User Profile</h2>
+          <form className="mt-4">
+            <p>
+              <img src={userData.avatarIMG}
+               className="img-fluid"
+               alt="User Avatar" />
+            </p>
+            <p>User Name: {userData.userName}</p>
+            <p>Email: {userData.email}</p>
+            <p>Phone Number: {userData.phoneNumber}</p>
+            <p>Address: {userData.address}</p>
+          </form>
         </div>
       </Row>
     </DashboardTemplate>
